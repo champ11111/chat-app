637faa3bd002c7dca764310bc94c7c2f625d76fa
Nattawat Kwansakul <champ_2543@hotmail.com>
2023-04-20 18:32:52 +0700

    feat: add connection to Postgres database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // change to Postgres
+      host: 'localhost',
+      username: 'user',
+      password: 'password',
+      database: 'chapabc',
+      port: 5432, // change port to default Postgres port
+      entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
