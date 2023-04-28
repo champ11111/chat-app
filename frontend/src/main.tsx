1cf58bd25fc1e7ed5b1343c933be6e35031f5e08
Bxntn <nuttanan118123@gmail.com>
2023-04-28 19:49:10 +0700

    feat: Add landing and chatroom page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import LandingPage from "./page/landing-page";
+import ErrorPage from "./page/error-page"
+import ChatPage from "./page/chat-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LandingPage />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/main",
+        element: <ChatPage />,
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
---
+++ frontend/src/page/chat-page.tsx
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react';
+import Sidebar from '../component/sidebar';
+import ProfileOverlay from '../component/profile-overlay';
+import Chatroom from '../component/chat-room';
+
+export default function ChatPage(){
+    return (
+        <div id="chat-page" className= "flex w">
+            <Sidebar/>
+            <div id = "main" className = "w-full">
+                <Chatroom/>
+            </div>
+        </div>
+    )
+}
