b1e4a76ebffa75c8fc62aac2168c326c3650ccd0
Bxntn <nuttanan118123@gmail.com>
2023-04-28 19:48:29 +0700

    feat: Add login and register component

---
+++ frontend/src/component/register.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+const RegisterCard: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = () => {
+    // TODO: Implement registration logic
+  };
+
+  return (
+    <div className="flex items-center justify-center bg-gray-100 space-y-auto text-center">
+      <div className="w-full max-w-md">
+        <div className="bg-white rounded-lg shadow-lg p-8">
+          <h1 className="text-2xl font-bold mb-4">Register</h1>
+          <div className="mb-4 text-left">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
+              Nickname
+            </label>
+            <input
+              className="border rounded-lg py-2 px-3 w-full"
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-4 text-left">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="border rounded-lg py-2 px-3 w-full"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4 text-left">
+            <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="border rounded-lg py-2 px-3 w-full"
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleRegister}
+          >
+            Register
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterCard;
