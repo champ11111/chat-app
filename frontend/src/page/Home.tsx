aa0a38b3ddd7d560172ccb5c4df7af653cefe09b
ChayutAu <modunkung@gmail.com>
2023-04-30 00:25:06 +0700

    Revert "Merge branch 'frontend' into main"
    This reverts commit e613269264dc0036b6e14f268551043f509ad504.

--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -1,5 +1,5 @@
 import React, {ReactNode, useState, useRef, useEffect} from "react";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo.jpg";
 import LoginModal from "../component/LoginModal";
 import RegisterModal from "../component/RegisterModal";
 
@@ -32,36 +32,34 @@ export default function Home(){
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-col items-center justify-center space-y-50">
                 <img src={logo} className="h-64 w-64" alt="logo" />
-                <h1 className="text-6xl font-normal dark:text-white transition duration-500">
+                <h1 className="text-6xl font-bold">
                     ChapABC
                 </h1>
                 
                 {/* login button */}
-                <div className="flex flex-col items-center justify-center my-10" >
-                    <button className="bg-gray-700 dark:bg-gray-300 hover:bg-dark dark:hover:bg-white text-white dark:text-gray-800 font-med py-2 px-20 rounded-full w-full text-center cursor-pointer transition duration-500 ease-in-out"
-                    onClick={()=>setIsLogin(!isLogin)}>Login</button>
+                <div className="flex flex-col items-center justify-center">
+                    <div className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full text-center cursor-pointer"
+                    onClick={()=>setIsLogin(!isLogin)}>Login</div>
                     {isLogin && (
-                        <div className="fixed top-0 left-0 w-full h-full opacity-50 z-10"></div>
+                        <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
                         )}
                         {isLogin && (
                             <div 
-                            className="fixed rounded-lg top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-gray-800 p-6 z-20" 
+                            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 z-20" 
                             ref = {overlayRef}>
                                 <LoginModal/>
                             </div>
                         )}
-                    <div className= "my-2"></div>
-                    <button className="bg-blue-500 dark:bg-blue-700 hover:bg-blue-500 dark:hover:bg-blue-900 text-white font-bold py-2 px-20 rounded-full text-center cursor-pointer transition duration-500 ease-in-out"
-                    onClick={()=>setIsRegister(!isRegister)}>Register</button>
+                    <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center cursor-pointer"
+                    onClick={()=>setIsRegister(!isRegister)}>Register</div>
                     {isRegister && (
-                        <div className="fixed top-0 left-0 w-full h-full opacity-50 z-10">
-                        </div>
+                        <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-10"></div>
                         )}
                         {isRegister && (
                             <div 
-                            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-gray-800 rounded-lg  p-6 z-20" 
+                            className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6 z-20" 
                             ref = {overlayRef}>
-                                <RegisterModal onClose={() => setIsRegister(false)}/>
+                                <RegisterModal/>
                             </div>
                         )}
                 </div>
